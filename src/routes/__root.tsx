eb1535db3c481ed6de30a1a699158dda207de4df
Refrescario <refrescario@gmail.com>
2026-05-30 15:02:19 -0300

    fix Vercel deploy with static SPA build

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -100,15 +100,10 @@ export const Route = createRootRouteWithContext<{ queryClient: QueryClient }>()(
 
 function RootShell({ children }: { children: React.ReactNode }) {
   return (
-    <html lang="en">
-      <head>
-        <HeadContent />
-      </head>
-      <body>
-        {children}
-        <Scripts />
-      </body>
-    </html>
+    <>
+      <HeadContent />
+      {children}
+    </>
   );
 }
 
